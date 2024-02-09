6d5394c2a334ecf5a4b51a631c78c702951101cc
Gaurang Patel <93378047+Patel-Gaurang-12@users.noreply.github.com>
2024-02-09 10:14:51 +0530

    'api'

--- main.js
+++ main.js
@@ -1,6 +1,390 @@
 // landing page
 var city_names = [
-	'Aberdeen', 'Abilene', 'Akron', 'Albany', 'Albuquerque', 'Alexandria', 'Allentown', 'Amarillo', 'Anaheim', 'Anchorage', 'Ann Arbor', 'Antioch', 'Apple Valley', 'Appleton', 'Arlington', 'Arvada', 'Asheville', 'Athens', 'Atlanta', 'Atlantic City', 'Augusta', 'Aurora', 'Austin', 'Bakersfield', 'Baltimore', 'Barnstable', 'Baton Rouge', 'Beaumont', 'Bel Air', 'Bellevue', 'Berkeley', 'Bethlehem', 'Billings', 'Birmingham', 'Bloomington', 'Boise', 'Boise City', 'Bonita Springs', 'Boston', 'Boulder', 'Bradenton', 'Bremerton', 'Bridgeport', 'Brighton', 'Brownsville', 'Bryan', 'Buffalo', 'Burbank', 'Burlington', 'Cambridge', 'Canton', 'Cape Coral', 'Carrollton', 'Cary', 'Cathedral City', 'Cedar Rapids', 'Champaign', 'Chandler', 'Charleston', 'Charlotte', 'Chattanooga', 'Chesapeake', 'Chicago', 'Chula Vista', 'Cincinnati', 'Clarke County', 'Clarksville', 'Clearwater', 'Cleveland', 'College Station', 'Colorado Springs', 'Columbia', 'Columbus', 'Concord', 'Coral Springs', 'Corona', 'Corpus Christi', 'Costa Mesa', 'Dallas', 'Daly City', 'Danbury', 'Davenport', 'Davidson County', 'Dayton', 'Daytona Beach', 'Deltona', 'Denton', 'Denver', 'Des Moines', 'Detroit', 'Downey', 'Duluth', 'Durham', 'El Monte', 'El Paso', 'Elizabeth', 'Elk Grove', 'Elkhart', 'Erie', 'Escondido', 'Eugene', 'Evansville', 'Fairfield', 'Fargo', 'Fayetteville', 'Fitchburg', 'Flint', 'Fontana', 'Fort Collins', 'Fort Lauderdale', 'Fort Smith', 'Fort Walton Beach', 'Fort Wayne', 'Fort Worth', 'Frederick', 'Fremont', 'Fresno', 'Fullerton', 'Gainesville', 'Garden Grove', 'Garland', 'Gastonia', 'Gilbert', 'Glendale', 'Grand Prairie', 'Grand Rapids', 'Grayslake', 'Green Bay', 'GreenBay', 'Greensboro', 'Greenville', 'Gulfport-Biloxi', 'Hagerstown', 'Hampton', 'Harlingen', 'Harrisburg', 'Hartford', 'Havre de Grace', 'Hayward', 'Hemet', 'Henderson', 'Hesperia', 'Hialeah', 'Hickory', 'High Point', 'Hollywood', 'Honolulu', 'Houma', 'Houston', 'Howell', 'Huntington', 'Huntington Beach', 'Huntsville', 'Independence', 'Indianapolis', 'Inglewood', 'Irvine', 'Irving', 'Jackson', 'Jacksonville', 'Jefferson', 'Jersey City', 'Johnson City', 'Joliet', 'Kailua', 'Kalamazoo', 'Kaneohe', 'Kansas City', 'Kennewick', 'Kenosha', 'Killeen', 'Kissimmee', 'Knoxville', 'Lacey', 'Lafayette', 'Lake Charles', 'Lakeland', 'Lakewood', 'Lancaster', 'Lansing', 'Laredo', 'Las Cruces', 'Las Vegas', 'Layton', 'Leominster', 'Lewisville', 'Lexington', 'Lincoln', 'Little Rock', 'Long Beach', 'Lorain', 'Los Angeles', 'Louisville', 'Lowell', 'Lubbock', 'Macon', 'Madison', 'Manchester', 'Marina', 'Marysville', 'McAllen', 'McHenry', 'Medford', 'Melbourne', 'Memphis', 'Merced', 'Mesa', 'Mesquite', 'Miami', 'Milwaukee', 'Minneapolis', 'Miramar', 'Mission Viejo', 'Mobile', 'Modesto', 'Monroe', 'Monterey', 'Montgomery', 'Moreno Valley', 'Murfreesboro', 'Murrieta', 'Muskegon', 'Myrtle Beach', 'Naperville', 'Naples', 'Nashua', 'Nashville', 'New Bedford', 'New Haven', 'New London', 'New Orleans', 'New York', 'New York City', 'Newark', 'Newburgh', 'Newport News', 'Norfolk', 'Normal', 'Norman', 'North Charleston', 'North Las Vegas', 'North Port', 'Norwalk', 'Norwich', 'Oakland', 'Ocala', 'Oceanside', 'Odessa', 'Ogden', 'Oklahoma City', 'Olathe', 'Olympia', 'Omaha', 'Ontario', 'Orange', 'Orem', 'Orlando', 'Overland Park', 'Oxnard', 'Palm Bay', 'Palm Springs', 'Palmdale', 'Panama City', 'Pasadena', 'Paterson', 'Pembroke Pines', 'Pensacola', 'Peoria', 'Philadelphia', 'Phoenix', 'Pittsburgh', 'Plano', 'Pomona', 'Pompano Beach', 'Port Arthur', 'Port Orange', 'Port Saint Lucie', 'Port St. Lucie', 'Portland', 'Portsmouth', 'Poughkeepsie', 'Providence', 'Provo', 'Pueblo', 'Punta Gorda', 'Racine', 'Raleigh', 'Rancho Cucamonga', 'Reading', 'Redding', 'Reno', 'Richland', 'Richmond', 'Richmond County', 'Riverside', 'Roanoke', 'Rochester', 'Rockford', 'Roseville', 'Round Lake Beach', 'Sacramento', 'Saginaw', 'Saint Louis', 'Saint Paul', 'Saint Petersburg', 'Salem', 'Salinas', 'Salt Lake City', 'San Antonio', 'San Bernardino', 'San Buenaventura', 'San Diego', 'San Francisco', 'San Jose', 'Santa Ana', 'Santa Barbara', 'Santa Clara', 'Santa Clarita', 'Santa Cruz', 'Santa Maria', 'Santa Rosa', 'Sarasota', 'Savannah', 'Scottsdale', 'Scranton', 'Seaside', 'Seattle', 'Sebastian', 'Shreveport', 'Simi Valley', 'Sioux City', 'Sioux Falls', 'South Bend', 'South Lyon', 'Spartanburg', 'Spokane', 'Springdale', 'Springfield', 'St. Louis', 'St. Paul', 'St. Petersburg', 'Stamford', 'Sterling Heights', 'Stockton', 'Sunnyvale', 'Syracuse', 'Tacoma', 'Tallahassee', 'Tampa', 'Temecula', 'Tempe', 'Thornton', 'Thousand Oaks', 'Toledo', 'Topeka', 'Torrance', 'Trenton', 'Tucson', 'Tulsa', 'Tuscaloosa', 'Tyler', 'Utica', 'Vallejo', 'Vancouver', 'Vero Beach', 'Victorville', 'Virginia Beach', 'Visalia', 'Waco', 'Warren', 'Washington', 'Waterbury', 'Waterloo', 'West Covina', 'West Valley City', 'Westminster', 'Wichita', 'Wilmington', 'Winston', 'Winter Haven', 'Worcester', 'Yakima', 'Yonkers', 'York', 'Youngstown',
+	'Aberdeen',
+	'Abilene',
+	'Akron',
+	'Albany',
+	'Albuquerque',
+	'Alexandria',
+	'Allentown',
+	'Amarillo',
+	'Anaheim'
+	, 'Anchorage'
+	, 'Ann Arbor'
+	, 'Antioch'
+	, 'Apple Valley'
+	, 'Appleton'
+	, 'Arlington'
+	, 'Arvada'
+	, 'Asheville'
+	, 'Athens'
+	, 'Atlanta'
+	, 'Atlantic City'
+	, 'Augusta'
+	, 'Aurora'
+	, 'Austin'
+	, 'Bakersfield'
+	, 'Baltimore'
+	, 'Barnstable'
+	, 'Baton Rouge'
+	, 'Beaumont'
+	, 'Bel Air'
+	, 'Bellevue'
+	, 'Berkeley'
+	, 'Bethlehem'
+	, 'Billings'
+	, 'Birmingham'
+	, 'Bloomington'
+	, 'Boise'
+	, 'Boise City'
+	, 'Bonita Springs'
+	, 'Boston'
+	, 'Boulder'
+	, 'Bradenton'
+	, 'Bremerton'
+	, 'Bridgeport'
+	, 'Brighton'
+	, 'Brownsville'
+	, 'Bryan'
+	, 'Buffalo'
+	, 'Burbank'
+	, 'Burlington'
+	, 'Cambridge'
+	, 'Canton'
+	, 'Cape Coral'
+	, 'Carrollton'
+	, 'Cary'
+	, 'Cathedral City'
+	, 'Cedar Rapids'
+	, 'Champaign'
+	, 'Chandler'
+	, 'Charleston'
+	, 'Charlotte'
+	, 'Chattanooga'
+	, 'Chesapeake'
+	, 'Chicago'
+	, 'Chula Vista'
+	, 'Cincinnati'
+	, 'Clarke County'
+	, 'Clarksville'
+	, 'Clearwater'
+	, 'Cleveland'
+	, 'College Station'
+	, 'Colorado Springs'
+	, 'Columbia'
+	, 'Columbus'
+	, 'Concord'
+	, 'Coral Springs'
+	, 'Corona'
+	, 'Corpus Christi'
+	, 'Costa Mesa'
+	, 'Dallas'
+	, 'Daly City'
+	, 'Danbury'
+	, 'Davenport'
+	, 'Davidson County'
+	, 'Dayton'
+	, 'Daytona Beach'
+	, 'Deltona'
+	, 'Denton'
+	, 'Denver'
+	, 'Des Moines'
+	, 'Detroit'
+	, 'Downey'
+	, 'Duluth'
+	, 'Durham'
+	, 'El Monte'
+	, 'El Paso'
+	, 'Elizabeth'
+	, 'Elk Grove'
+	, 'Elkhart'
+	, 'Erie'
+	, 'Escondido'
+	, 'Eugene'
+	, 'Evansville'
+	, 'Fairfield'
+	, 'Fargo'
+	, 'Fayetteville'
+	, 'Fitchburg'
+	, 'Flint'
+	, 'Fontana'
+	, 'Fort Collins'
+	, 'Fort Lauderdale'
+	, 'Fort Smith'
+	, 'Fort Walton Beach'
+	, 'Fort Wayne'
+	, 'Fort Worth'
+	, 'Frederick'
+	, 'Fremont'
+	, 'Fresno'
+	, 'Fullerton'
+	, 'Gainesville'
+	, 'Garden Grove'
+	, 'Garland'
+	, 'Gastonia'
+	, 'Gilbert'
+	, 'Glendale'
+	, 'Grand Prairie'
+	, 'Grand Rapids'
+	, 'Grayslake'
+	, 'Green Bay'
+	, 'GreenBay'
+	, 'Greensboro'
+	, 'Greenville'
+	, 'Gulfport-Biloxi'
+	, 'Hagerstown'
+	, 'Hampton'
+	, 'Harlingen'
+	, 'Harrisburg'
+	, 'Hartford'
+	, 'Havre de Grace'
+	, 'Hayward'
+	, 'Hemet'
+	, 'Henderson'
+	, 'Hesperia'
+	, 'Hialeah'
+	, 'Hickory'
+	, 'High Point'
+	, 'Hollywood'
+	, 'Honolulu'
+	, 'Houma'
+	, 'Houston'
+	, 'Howell'
+	, 'Huntington'
+	, 'Huntington Beach'
+	, 'Huntsville'
+	, 'Independence'
+	, 'Indianapolis'
+	, 'Inglewood'
+	, 'Irvine'
+	, 'Irving'
+	, 'Jackson'
+	, 'Jacksonville'
+	, 'Jefferson'
+	, 'Jersey City'
+	, 'Johnson City'
+	, 'Joliet'
+	, 'Kailua'
+	, 'Kalamazoo'
+	, 'Kaneohe'
+	, 'Kansas City'
+	, 'Kennewick'
+	, 'Kenosha'
+	, 'Killeen'
+	, 'Kissimmee'
+	, 'Knoxville'
+	, 'Lacey'
+	, 'Lafayette'
+	, 'Lake Charles'
+	, 'Lakeland'
+	, 'Lakewood'
+	, 'Lancaster'
+	, 'Lansing'
+	, 'Laredo'
+	, 'Las Cruces'
+	, 'Las Vegas'
+	, 'Layton'
+	, 'Leominster'
+	, 'Lewisville'
+	, 'Lexington'
+	, 'Lincoln'
+	, 'Little Rock'
+	, 'Long Beach'
+	, 'Lorain'
+	, 'Los Angeles'
+	, 'Louisville'
+	, 'Lowell'
+	, 'Lubbock'
+	, 'Macon'
+	, 'Madison'
+	, 'Manchester'
+	, 'Marina'
+	, 'Marysville'
+	, 'McAllen'
+	, 'McHenry'
+	, 'Medford'
+	, 'Melbourne'
+	, 'Memphis'
+	, 'Merced'
+	, 'Mesa'
+	, 'Mesquite'
+	, 'Miami'
+	, 'Milwaukee'
+	, 'Minneapolis'
+	, 'Miramar'
+	, 'Mission Viejo'
+	, 'Mobile'
+	, 'Modesto'
+	, 'Monroe'
+	, 'Monterey'
+	, 'Montgomery'
+	, 'Moreno Valley'
+	, 'Murfreesboro'
+	, 'Murrieta'
+	, 'Muskegon'
+	, 'Myrtle Beach'
+	, 'Naperville'
+	, 'Naples'
+	, 'Nashua'
+	, 'Nashville'
+	, 'New Bedford'
+	, 'New Haven'
+	, 'New London'
+	, 'New Orleans'
+	, 'New York'
+	, 'New York City'
+	, 'Newark'
+	, 'Newburgh'
+	, 'Newport News'
+	, 'Norfolk'
+	, 'Normal'
+	, 'Norman'
+	, 'North Charleston'
+	, 'North Las Vegas'
+	, 'North Port'
+	, 'Norwalk'
+	, 'Norwich'
+	, 'Oakland'
+	, 'Ocala'
+	, 'Oceanside'
+	, 'Odessa'
+	, 'Ogden'
+	, 'Oklahoma City'
+	, 'Olathe'
+	, 'Olympia'
+	, 'Omaha'
+	, 'Ontario'
+	, 'Orange'
+	, 'Orem'
+	, 'Orlando'
+	, 'Overland Park'
+	, 'Oxnard'
+	, 'Palm Bay'
+	, 'Palm Springs'
+	, 'Palmdale'
+	, 'Panama City'
+	, 'Pasadena'
+	, 'Paterson'
+	, 'Pembroke Pines'
+	, 'Pensacola'
+	, 'Peoria'
+	, 'Philadelphia'
+	, 'Phoenix'
+	, 'Pittsburgh'
+	, 'Plano'
+	, 'Pomona'
+	, 'Pompano Beach'
+	, 'Port Arthur'
+	, 'Port Orange'
+	, 'Port Saint Lucie'
+	, 'Port St. Lucie'
+	, 'Portland'
+	, 'Portsmouth'
+	, 'Poughkeepsie'
+	, 'Providence'
+	, 'Provo'
+	, 'Pueblo'
+	, 'Punta Gorda'
+	, 'Racine'
+	, 'Raleigh'
+	, 'Rancho Cucamonga'
+	, 'Reading'
+	, 'Redding'
+	, 'Reno'
+	, 'Richland'
+	, 'Richmond'
+	, 'Richmond County'
+	, 'Riverside'
+	, 'Roanoke'
+	, 'Rochester'
+	, 'Rockford'
+	, 'Roseville'
+	, 'Round Lake Beach'
+	, 'Sacramento'
+	, 'Saginaw'
+	, 'Saint Louis'
+	, 'Saint Paul'
+	, 'Saint Petersburg'
+	, 'Salem'
+	, 'Salinas'
+	, 'Salt Lake City'
+	, 'San Antonio'
+	, 'San Bernardino'
+	, 'San Buenaventura'
+	, 'San Diego'
+	, 'San Francisco'
+	, 'San Jose'
+	, 'Santa Ana'
+	, 'Santa Barbara'
+	, 'Santa Clara'
+	, 'Santa Clarita'
+	, 'Santa Cruz'
+	, 'Santa Maria'
+	, 'Santa Rosa'
+	, 'Sarasota'
+	, 'Savannah'
+	, 'Scottsdale'
+	, 'Scranton'
+	, 'Seaside'
+	, 'Seattle'
+	, 'Sebastian'
+	, 'Shreveport'
+	, 'Simi Valley'
+	, 'Sioux City'
+	, 'Sioux Falls'
+	, 'South Bend'
+	, 'South Lyon'
+	, 'Spartanburg'
+	, 'Spokane'
+	, 'Springdale'
+	, 'Springfield'
+	, 'St. Louis'
+	, 'St. Paul'
+	, 'St. Petersburg'
+	, 'Stamford'
+	, 'Sterling Heights'
+	, 'Stockton'
+	, 'Sunnyvale'
+	, 'Syracuse'
+	, 'Tacoma'
+	, 'Tallahassee'
+	, 'Tampa'
+	, 'Temecula'
+	, 'Tempe'
+	, 'Thornton'
+	, 'Thousand Oaks'
+	, 'Toledo'
+	, 'Topeka'
+	, 'Torrance'
+	, 'Trenton'
+	, 'Tucson'
+	, 'Tulsa'
+	, 'Tuscaloosa'
+	, 'Tyler'
+	, 'Utica'
+	, 'Vallejo'
+	, 'Vancouver'
+	, 'Vero Beach'
+	, 'Victorville'
+	, 'Virginia Beach'
+	, 'Visalia'
+	, 'Waco'
+	, 'Warren'
+	, 'Washington'
+	, 'Waterbury'
+	, 'Waterloo'
+	, 'West Covina'
+	, 'West Valley City'
+	, 'Westminster'
+	, 'Wichita'
+	, 'Wilmington'
+	, 'Winston'
+	, 'Winter Haven'
+	, 'Worcester'
+	, 'Yakima'
+	, 'Yonkers'
+	, 'York'
+	, 'Youngstown',
 	'Adilabad',
 	'Anantapur',
 	'Chittoor',
@@ -363,8 +747,10 @@ var city_names = [
 	'use strict';
 	let inputField = document.getElementById('input');
 	let ulField = document.getElementById('suggestions');
-	inputField.addEventListener('input', changeAutoComplete);
-	ulField.addEventListener('click', selectItem);
+	inputField.addEventListener('input'
+		, changeAutoComplete);
+	ulField.addEventListener('click'
+		, selectItem);
 
 	function changeAutoComplete({ target }) {
 		let data = target.value;
@@ -415,19 +801,20 @@ async function buttenClick() {
 
 async function charge(name) {
 	let apidata;
-	await fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=' + name + '&format=json&u=c', {
-		method: 'GET',	
-		headers: {
-			'X-RapidAPI-Key': 'b3efe5c804mshcd16ae5c6e3143ap1ad18ejsn90756071c0f8',
-			'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
-		}
-	}).then(responce => {
-		return responce.json();
-	}).then(data => {
-		apidata = data;
-	}).catch(err => {
-		console.log('Error ocurr in first function,' + err);
-	});
+	await fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=' + name + '&format=json&u=c'
+		, {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': 'your api key',
+				'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+			}
+		}).then(responce => {
+			return responce.json();
+		}).then(data => {
+			apidata = data;
+		}).catch(err => {
+			console.log('Error ocurr in first function,' + err);
+		});
 	return apidata;
 }
 
@@ -445,32 +832,33 @@ function actionValue(data) {
 	document.getElementById("app-set").innerHTML = data.current_observation.astronomy.sunset;
 	document.getElementById("app-direction").innerHTML = data.current_observation.wind.direction;
 	document.getElementById("app-speed").innerHTML = data.current_observation.wind.speed;
-	document.getElementById("app-temp").insertAdjacentHTML("beforeend", " °C");
-	document.getElementById("app-humi").insertAdjacentHTML("beforeend", " %");
-	document.getElementById("app-pre").insertAdjacentHTML("beforeend", " mb");
-	document.getElementById("app-direction").insertAdjacentHTML("beforeend", "°");
-	document.getElementById("app-speed").insertAdjacentHTML("beforeend", " km/hr");
+	document.getElementById("app-temp").insertAdjacentHTML("beforeend"
+		, " °C");
+	document.getElementById("app-humi").insertAdjacentHTML("beforeend"
+		, " %");
+	document.getElementById("app-pre").insertAdjacentHTML("beforeend"
+		, " mb");
+	document.getElementById("app-direction").insertAdjacentHTML("beforeend"
+		, "°");
+	document.getElementById("app-speed").insertAdjacentHTML("beforeend"
+		, " km/hr");
 
 	// Chat
 
 	let days = [];
 	days[0] = "today";
 	days[1] = "yesterday";
-	for(let i=2; i<data.forecasts.length; i++)
-	{
+	for (let i = 2; i < data.forecasts.length; i++) {
 		days[i] = data.forecasts[i].day;
 	}
 
 	let minValue = data.forecasts[0].high;
 	let maxvalue = data.forecasts[0].low;
-	for(let i=1; i<data.forecasts.length; i++)
-	{
-		if(minValue > data.forecasts[i].low)
-		{
+	for (let i = 1; i < data.forecasts.length; i++) {
+		if (minValue > data.forecasts[i].low) {
 			minValue = data.forecasts[i].low;
 		}
-		if(maxvalue <  data.forecasts[i].high)
-		{
+		if (maxvalue < data.forecasts[i].high) {
 			maxvalue = data.forecasts[i].high;
 		}
 	}
@@ -485,31 +873,33 @@ function actionValue(data) {
 		lowTemp.push(value.low);
 	});
 
-	new Chart("myChart", {
-		type: "line",
-		data: {
-			labels: xValues,
-			datasets: [{
-				data: highTemp,
-				borderColor: "#ff4848c9",
-				fill: false
-			}, {
-				data: lowTemp,
-				borderColor: "#0013ffc7",
-				fill: false
-			}]
-		},
-		options: {
-			legend: { display: false },
-			scales: {
-				yAxes: [{
-					display: true,
-					ticks: {
-						min: minValue,
-						max: maxvalue 
-					}
+	new Chart("myChart"
+		, {
+			type: "line",
+			data: {
+				labels: xValues,
+				datasets: [{
+					data: highTemp,
+					borderColor: "#ff4848c9",
+					fill: false
+				}
+					, {
+					data: lowTemp,
+					borderColor: "#0013ffc7",
+					fill: false
 				}]
 			},
-		}
-	});
+			options: {
+				legend: { display: false },
+				scales: {
+					yAxes: [{
+						display: true,
+						ticks: {
+							min: minValue,
+							max: maxvalue
+						}
+					}]
+				},
+			}
+		});
 }
